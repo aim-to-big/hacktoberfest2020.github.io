f6064ff71ba93b20158bd2fa2202fcbecb2840c8
aim-to-big <72402548+aim-to-big@users.noreply.github.com>
2020-10-05 22:57:20 +0530

    wants update

---
+++ if_else.py
@@ -0,0 +1,32 @@
+# initialize variable
+num = 5
+condition = None
+
+# Code starts here
+
+# check for conditions
+if(num>0):
+    print('num is positive')
+elif(num<0):
+    print('num is negative')
+else:
+    print('num is zero')
+# Code ends here
+#on more example!
+# initialize variable
+num = 5
+condition = None
+
+# Code starts here
+
+# check for conditions
+#if num>0:
+#    condition = 'positive'
+#    print('num is positive')
+#elif num<0:
+#    condition('negative')
+#    print('num is negative')
+#else:
+#    condition('zero')
+#    print('num is zero')
+# Code ends here
